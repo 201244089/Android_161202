cebdad6eba534ecb30112bd89de54b284369860d
LBH <dlqudgml0503@naver.com>
2016-12-02 12:39:28 +0900

    Complete

--- app/src/main/java/sampleandroid/reservation/MainActivity.java
+++ app/src/main/java/sampleandroid/reservation/MainActivity.java
@@ -1,6 +1,7 @@
 package sampleandroid.reservation;
 
 import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,22 +25,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv1.findViewById(R.id.textView);
-        tv2.findViewById(R.id.textView2);
-        chmeter.findViewById(R.id.chronometer1);
-        b_start.findViewById(R.id.b_start);
-        b_stop.findViewById(R.id.b_stop);
-        rg.findViewById(R.id.Radiogroup);
-        r_cal.findViewById(R.id.r_calender);
-        r_time.findViewById(R.id.r_time);
-        calView.findViewById(R.id.calendarView2);
-        tP.findViewById(R.id.timePicker2);
+        tv1 = (TextView)findViewById(R.id.textView);
+        tv2 = (TextView)findViewById(R.id.textView2);
+        chmeter = (Chronometer)findViewById(R.id.chronometer1);
+        b_start =(Button)findViewById(R.id.b_start);
+        b_stop= (Button)findViewById(R.id.b_stop);
+        rg=(RadioGroup)findViewById(R.id.Radiogroup);
+        r_cal=(RadioButton)findViewById(R.id.r_calender);
+        r_time=(RadioButton)findViewById(R.id.r_time);
+        calView=(CalendarView)findViewById(R.id.calendarView2);
+        tP=(TimePicker)findViewById(R.id.timePicker2);
 
         b_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv1.setTextColor(Color.RED);
+                chmeter.setBase(SystemClock.elapsedRealtime());
                 chmeter.start();
+
             }
         });
 
