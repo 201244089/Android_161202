2c5b645eabe44777557415f63b51e95b5351cb8d
LBH <dlqudgml0503@naver.com>
2016-12-02 11:47:12 +0900

    객체 생성

--- app/src/main/java/sampleandroid/reservation/MainActivity.java
+++ app/src/main/java/sampleandroid/reservation/MainActivity.java
@@ -6,17 +6,29 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_1, tv_2;
+    TextView tv2;
     Chronometer chmeter;
     Button b_start, b_stop;
     RadioGroup rg;
     RadioButton r_cal, r_time;
     CalendarView calView;
-    TimePicker Tp;
+    TimePicker tP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv2.findViewById(R.id.textView2);
+        chmeter.findViewById(R.id.chronometer1);
+        b_start.findViewById(R.id.b_start);
+        b_stop.findViewById(R.id.b_stop);
+        rg.findViewById(R.id.Radiogroup);
+        r_cal.findViewById(R.id.r_calender);
+        r_time.findViewById(R.id.r_time);
+        calView.findViewById(R.id.calendarView2);
+        tP.findViewById(R.id.timePicker2);
+
+
     }
 }
