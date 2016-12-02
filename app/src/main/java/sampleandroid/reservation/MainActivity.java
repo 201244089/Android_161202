2d459b8e18f72eed0bf00cd1701e975cce8b9b4c
LBH <dlqudgml0503@naver.com>
2016-12-02 12:02:35 +0900

    예약시잔 버튼 누르면 텍스트 붉은색으로 바뀌고
    chronometer 시작
    
    라디오 버튼에 따라서 보여주는 컴포넌트 바꿈

--- app/src/main/java/sampleandroid/reservation/MainActivity.java
+++ app/src/main/java/sampleandroid/reservation/MainActivity.java
@@ -1,12 +1,14 @@
 package sampleandroid.reservation;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv2;
+    TextView tv1, tv2;
     Chronometer chmeter;
     Button b_start, b_stop;
     RadioGroup rg;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv1.findViewById(R.id.textView);
         tv2.findViewById(R.id.textView2);
         chmeter.findViewById(R.id.chronometer1);
         b_start.findViewById(R.id.b_start);
@@ -28,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
         r_time.findViewById(R.id.r_time);
         calView.findViewById(R.id.calendarView2);
         tP.findViewById(R.id.timePicker2);
+
+        b_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv1.setTextColor(Color.RED);
+                chmeter.start();
+            }
+        });
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(r_cal.isChecked()){
+                    calView.setVisibility(View.VISIBLE);
+                    tP.setVisibility(View.INVISIBLE);
+                }
+                if(r_time.isChecked()){
+                    calView.setVisibility(View.INVISIBLE);
+                    tP.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 
     }
