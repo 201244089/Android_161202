9d1e889402a006f53b888bdb2b7c7c35e909ddc2
LBH <dlqudgml0503@naver.com>
2016-12-02 11:42:02 +0900

    변수 설정

--- app/src/main/java/sampleandroid/reservation/MainActivity.java
+++ app/src/main/java/sampleandroid/reservation/MainActivity.java
@@ -2,8 +2,17 @@ package sampleandroid.reservation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_1, tv_2;
+    Chronometer chmeter;
+    Button b_start, b_stop;
+    RadioGroup rg;
+    RadioButton r_cal, r_time;
+    CalendarView calView;
+    TimePicker Tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
